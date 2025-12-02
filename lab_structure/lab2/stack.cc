66a7448239e04a643e76322b39ce16c2de608e9a
egorzhirkov146-ctrl <egorzhirkov146@gmail.com>
2025-12-02 21:09:43 +0300

    Update stack.cc

--- lab_structure/lab2/stack.cc
+++ lab_structure/lab2/stack.cc
@@ -1,39 +1,78 @@
 #include "stack.h"
-#include <stdexcept>
+#include <type_traits>
 
-Stack::Stack(std::int64_t maxSize) : maxSize(maxSize) {}
 
-void Stack::push(float value) {
-    if (array.getSize() >= maxSize) { 
-        throw std::overflow_error("Stack overflow");
+template<typename T>
+Stack<T>::Stack(std::int64_t max_size) : max_size(max_size) {
+    if (max_size <= 0) {
+        throw std::invalid_argument("Stack size must be positive");
     }
-    array.push_back(value);
 }
 
-float Stack::top() {
-    if (array.isEmpty()) {
-        throw std::out_of_range("Stack is empty");
-    }
-    return array.back();
+template<typename T>
+float Stack<T>::convertToFloat(const T& value) const {
+    return static_cast<float>(value);
 }
 
-float Stack::pop() {
-    if (array.isEmpty()) {
+template<typename T>
+T Stack<T>::convertFromFloat(float value) const {
+    return static_cast<T>(value);
+}
+
+template<typename T>
+void Stack<T>::push(const T& value) {
+    checkOverflow();
+    array.push_back(convertToFloat(value));
+}
+
+template<typename T>
+T Stack<T>::top() const {
+    if (isEmpty()) {
         throw std::out_of_range("Stack is empty");
     }
-    float value = array.back();
-    array.delete_element(array.getSize() - 1); 
+    return convertFromFloat(array[0]);
+}
+
+template<typename T>
+T Stack<T>::pop() {
+    if (isEmpty()) {
+        throw std::out_of_range("Stack is empty");
+    }
+    T value = top();
+    array.pop_back();
     return value;
 }
 
-bool Stack::isEmpty() {
+template<typename T>
+bool Stack<T>::isEmpty() const {
     return array.isEmpty();
 }
 
-std::int64_t Stack::size() {
-    return array.getSize(); 
+template<typename T>
+bool Stack<T>::isFull() const {
+    return size() >= max_size;
 }
 
-std::int64_t Stack::getMaxSize() {
-    return maxSize;
+template<typename T>
+std::int64_t Stack<T>::size() const {
+    return array.getSize() + 1;
 }
+
+template<typename T>
+std::int64_t Stack<T>::getMaxSize() const {
+    return max_size;
+}
+
+template<typename T>
+void Stack<T>::checkOverflow() const {
+    if (isFull()) {
+        throw std::overflow_error("Stack overflow: maximum size reached");
+    }
+}
+
+template class Stack<int>;
+template class Stack<float>;
+template class Stack<double>;
+template class Stack<char>;
+template class Stack<long>;
+template class Stack<short>;
